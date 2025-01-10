342309a4c4addb99d391050e87d742b20a7c4bdf
Sachnun Kusmayadi <sachnun12@gmail.com>
2025-01-10 19:59:39 +0700

    Add GitHub Pages deployment workflow and update site configuration
    - Created a GitHub Actions workflow for deploying to GitHub Pages.
    - Updated Astro configuration with site URL and base path.
    - Modified layout and index files for title change and testing purposes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://sachnun.github.io",
+  base: "sachnun.github.io",
+});
